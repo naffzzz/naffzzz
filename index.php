1412bdd9cdd55006b2ec5627b739ee84cfe7a27d
unknown <naffzzz@gmail.com>
2023-01-29 09:18:50 +0700

    penambahan halaman web

---
+++ index.php
@@ -0,0 +1,424 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="fontawesome/css/fontawesome.css">
+    <link rel="stylesheet" href="fontawesome/css/brands.css">
+    <link rel="stylesheet" href="fontawesome/css/solid.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Nafa Zulfa</title>
+  </head>
+  <body class="body-background" onload="reveal()">
+    <!-- 4 - contained in center example -->
+    <nav class="navbar navbar-expand-sm navbar-light bg-white fixed-top slideInFromTopDef">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample11" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+      </button>
+        <div class="mx-auto d-sm-flex d-block flex-sm-nowrap">
+            <div class="collapse navbar-collapse text-center" id="navbarsExample11">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" id="biographyBtn">Biography</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" id="educationBtn">Education</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" id="workExperiencesBtn">Work Experiences</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" id="programmingBtn">Programmings</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" id="projectExperiencesBtn">Projects</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" id="publicationsBtn">Publications</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" id="awardsBtn">Awards</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" id="contactMeBtn">Contact Me</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <hr>
+
+    <div class="container mt-5">
+      <center class="reveal slideInFromBottom" id="biography"><h1 class="subtitle2">Biography <i class="fa-solid fa-briefcase"></i></h1></center>
+      <div class="row mb-5 reveal slideInFromBottom">
+        <div class="col bg-white text-justify">
+          <span>Nafa Zulfa was born in Kediri, Indonesia. He received the Bachelor degree in informatic engineering education from Malang State University (UM), Malang, Indonesia, and the Master degree in informatic engineering from Sepuluh Nopember Institut of Technology (ITS), Surabaya, Indonesia.
+            Currently he is a programmer at the Housestack, Indonesia. He is also interest in research with mainly focus on deep learning and computer vision.</span>
+        </div>
+      </div>
+
+      <center class="reveal slideInFromBottom" id="education">
+        <h1 class="subtitle">Education <i class="fa-solid fa-school"></i></h1>
+      </center>
+      <div class="row text-center mb-5">
+        <div class="col">
+          <div class="card border rounded reveal slideInFromLeft">
+            <div class="text-info">
+              <span class="fa-stack fa-4x mb-2 mt-2">
+                <i class="fa fa-circle fa-stack-2x icon-background"></i>
+                <i class="fa-solid fa-graduation-cap  fa-stack-1x"></i>
+              </span>
+            </div>
+            <h4>Bachelor Degree</h4>
+            August 2014 - August 2018<br>
+            Informatic Engineering Education, Malang State University
+          </div>
+        </div>
+        <div class="col">
+          <div class="card border rounded reveal slideInFromRight">
+            <div class="text-info">
+              <span class="fa-stack fa-4x mb-2 mt-2">
+                <i class="fa fa-circle fa-stack-2x icon-background"></i>
+                <i class="fa-solid fa-graduation-cap  fa-stack-1x"></i>
+              </span>
+            </div>
+            <h4>Master Degree</h4>
+            September 2019 - September 2021<br>
+            Informatic Engineering, Sepuluh Nopember Institut of Technology
+          </div>
+        </div>
+        </div>
+
+      <center id="workExperiences" class="reveal slideInFromBottom"><h1 class="subtitle2">Work Experiences <i class="fa-solid fa-briefcase"></i></h1></center>
+      <div class="row text-center mb-5 reveal slideInFromBottom">
+        <div class="col bg-white">
+          <div class="d-flex justify-content-between">
+            <div>
+              <span><i class="fa-solid fa-user-gear text-info"></i> Back End Engineer</span>
+            </div>
+            <div>
+              <span>July 2022 - Present</span>
+            </div>
+          </div>
+          <div class="d-flex justify-content-between">
+            <div>
+              <span>DOT Indonesia</span>
+            </div>
+            <div>
+              <span>Malang, Indonesia</span>
+            </div>
+          </div>
+          <hr>
+          <div class="d-flex justify-content-between">
+            <div>
+              <span><i class="fa-solid fa-user-gear text-info"></i> Programmer</span>
+            </div>
+            <div>
+              <span>June 2019 - July 2022</span>
+            </div>
+          </div>
+          <div class="d-flex justify-content-between">
+            <div>
+              <span>Housestack</span>
+            </div>
+            <div>
+              <span>Malang, Indonesia</span>
+            </div>
+          </div>
+          <hr>
+          <div class="d-flex justify-content-between">
+            <div>
+              <span><i class="fa-solid fa-chalkboard-user text-success"></i> Intern Teacher</span>
+            </div>
+            <div>
+              <span>Sept 2017 - October 2017</span>
+            </div>
+          </div>
+          <div class="d-flex justify-content-between">
+            <div>
+              <span>SMKN 2 Singosari</span>
+            </div>
+            <div>
+              <span>Malang, Indonesia</span>
+            </div>
+          </div>
+          <hr>
+          <div class="d-flex justify-content-between">
+            <div>
+              <span><i class="fa-solid fa-user-tie text-success"></i> Intern Employee</span>
+            </div>
+            <div>
+              <span>May 2017 - July 2017</span>
+            </div>
+          </div>
+          <div class="d-flex justify-content-between">
+            <div>
+              <span>Religion Ministry</span>
+            </div>
+            <div>
+              <span>Kediri, Indonesia</span>
+            </div>
+          </div>
+          <hr>
+        </div>
+      </div>
+
+      <center id="programming" class="reveal slideInFromBottom"><h1 class="subtitle">Programming <i class="fa-solid fa-code"></i></h1></center>
+      <div class="row text-justify mb-5 reveal slideInFromBottom">
+        <div class="col bg-white">
+          <div>
+            <span><strong>Web Programming:</strong> Laravel, ReactJS, CodeIgniter, Node.js, php, javascript, jQuery, AngularJS, Materialize, FontAwesome, Bootstrap, DotNet, GoLang</span>
+          </div>
+          <hr>
+          <div>
+            <span><strong>Research Programming:</strong> Python</span>
+          </div>
+          <hr>
+          <div>
+            <span><strong>Game Programming:</strong> C#</span>
+          </div>
+          <hr>
+          <div>
+            <span><strong>Another Programming:</strong> C++, C, .NET, Java</span>
+          </div>
+          <hr>
+          <div>
+            <span><strong>Database:</strong> MySQL</span>
+          </div>
+          <hr>
+        </div>
+      </div>
+
+      <center id="projectExperiences" class="reveal slideInFromBottom"><h1 class="subtitle">Project Experiences <i class="fa-solid fa-laptop-code"></i></h1></center>
+      <div class="row text-justify mb-5 reveal slideInFromBottom">
+        <div class="col bg-white">
+          <div>
+            <span><strong>Pama:</strong> E-Budgeting</span>
+          </div>
+          <hr>
+          <div>
+            <span><strong>ISIBOX:</strong> Marketplace</span>
+          </div>
+          <hr>
+          <div>
+            <span><strong>AKSENS:</strong> Aplikasi konseling kesehatan mental siswa </span>
+          </div>
+          <hr>
+          <div>
+            <span>Deteksi salah ketik berbasis web</span>
+          </div>
+          <hr>
+          <div>
+            <span>Sistem kelas online Genta English Course</span>
+          </div>
+          <hr>
+          <div>
+            <span><strong>ROSN1B:</strong> raport online SMKN 1 Blitar</span>
+          </div>
+          <hr>
+          <div>
+            <span><strong>GAMIFIKASI: </strong>Sistem pembelajaran biologi bermuatan gamifikasi berbasis web</span>
+          </div>
+          <hr>
+          <div>
+            <span><strong>INTAN:</strong> Sistem rekomendasi gangguan indihome</span>
+          </div>
+          <hr>
+        </div>
+      </div>
+
+      <center id="publications" class="reveal slideInFromBottom"><h1 class="subtitle2">Research <i class="fa-solid fa-magnifying-glass"></i></h1></center>
+      <div class="row text-justify mb-5 reveal slideInFromBottom">
+        <div class="col bg-white">
+          <div>
+            <span>Shintami Chusnul Hidayati, Yeni Anistyasari, <strong>Nafa Zulfa</strong>, Pima Hani Safitri,</span>
+            <span>"Exploring the Potential of Adopting Computer-graphics Animation to the Switch to a Plant-Based Diet".</span>
+            2022 Fifth International Conference on Vocational Education and Electrical Engineering (ICVEE),
+            Surabaya, Indonesia
+          </div>
+          <hr>
+          
+          <div>
+            <span><strong>Nafa Zulfa</strong>, Pima Hani Safitri,</span>
+            <span>"Optimize facial expression classification using geometric feature and machine learning".</span>
+            2021 7th International Conference on Electrical, Electronics and Information Engineering (ICEEIE),
+            Malang, Indonesia
+          </div>
+          <hr>
+
+          <div>
+              <span>Dini Yuniasri, <strong>Nafa Zulfa</strong>, Darlis Herumurti,</span>
+              <span>"The Effect of Visual Reward and Punishment in Mobile Game on Game Experience".</span>
+              2021 International Seminar on Application for Technology of Information and Communication (iSemantic),
+              Semarang, Indonesia
+            </div>
+          <hr>
+
+          <div>
+              <span><strong>Nafa Zulfa</strong>,</span>
+              <span>"Studi Kinerja Pembacaan Bahasa Bibir Menggunakan Convolutional Neural Network, Mel-Frequency Ceptrums Coefficient, Long Short-Term Memory, dan Gated Recurrent Units".</span>
+              Institut Teknologi Sepuluh Nopember,
+              Surabaya, Indonesia
+            </div>
+          <hr>
+
+          <div>
+              <span><strong>Nafa Zulfa</strong>, Nanik Suciati, Shintami Chusnul Hidayati,</span>
+              <span>"Improved Lip-reading Language using Gated Recurrent Units"</span>
+              JUTI: Jurnal Ilmiah Teknologi Informasi,
+              Surabaya, Indonesia
+            </div>
+          <hr>
+
+          <div>
+              <span>Rakha Asyrofi, <strong>Nafa Zulfa</strong>,</span>
+              <span>"CLOUDITY: Cloud supply chain framework design based on JUGO and blockchain".</span>
+              2020 6th Information Technology International Seminar (ITIS),
+              Surabaya, Indonesia
+            </div>
+          <hr>
+
+          <div>
+              <span><strong>Nafa Zulfa</strong>, Dini Yuniasri, Putri Damayanti, Darlis Herumurti, Andhik Ampuh Yunanto,</span>
+              <span>"The Effect of UI and UX Enhancement on Bomberman Game Based on Game Experience Questionnaire (GEQ)".</span>
+              2020 International Seminar on Application for Technology of Information and Communication (iSemantic),
+              Semarang, Indonesia
+            </div>
+          <hr>
+
+          <div>
+              <span><strong>Nafa Zulfa</strong>, Hafara Firdausi, Rakha Asyrofi,</span>
+              <span>"CLOUTIDY: A Cloud-based Supply Chain Management System using SEMAR and Blockchain System".</span>
+              JUTI: Jurnal Ilmiah Teknologi Informasi,
+              Semarang, Indonesia
+            </div>
+          <hr>
+
+          <div>
+              <span><strong>Nafa Zulfa</strong>,</span>
+              <span>"Pengembangan media pembelajaran interaktif pemrograman berorientasi objek bermuatan model pembelajaran teams games tournament untuk meningkatkan hasil belajar siswa kelas XI RPL SMKN 2 Singosari Kabupaten Malang"</span>
+              Universitas Negeri Malang,
+              Malang, Indonesia
+            </div>
+          <hr>
+        </div>
+      </div>
+
+      <center id="awards" class="reveal slideInFromBottom"><h1 class="subtitle">Awards <i class="fa-solid fa-award"></i></h1></center>
+      <div class="row text-justify mb-5 reveal slideInFromBottom">
+        <div class="col bg-white">
+          <div>
+            <span>2021 <strong>Graduated Cum Laude with honors</strong> from Informatic Engineering Master Degree in Sepuluh Nopember Institut of Technology.</span>
+          </div>
+          <hr>
+          <div>
+            <span>2018 <strong>Best speaker of article presentation</strong> at Genta English Course in Pare, Kediri.</span>
+          </div>
+          <hr>
+          <div>
+            <span>2018 <strong>Graduated Cum Laude with honors</strong> from Informatic Engineering Education Bachelor Degree in Malang State University.</span>
+          </div>
+          <hr>
+          <div>
+            <span>2009 <strong>First rank</strong> at National Exam Olympiad in Islamic Junior High School 4 Kediri.</span>
+          </div>
+          <hr>
+        </div>
+      </div>
+
+    </div>
+
+    <footer class="footer1 pb-5 pt-5">
+      <div id="contactMe" class="container">
+        <center>
+          <a href="https://www.facebook.com/naff.sugaku/" style="text-decoration: none;" class="text-white" target="_blank"><i class="fa-brands fa-facebook fa-xl"></i> Facebook </a>
+          <a href="https://www.instagram.com/zulfa_nafa/" style="text-decoration: none;" class="text-white" target="_blank"><i class="fa-brands fa-instagram fa-xl"></i> Instagram </a>
+          <a href="https://www.linkedin.com/in/nafa-zulfa-ab85a1119/" style="text-decoration: none;" class="text-white" target="_blank"><i class="fa-brands fa-linkedin fa-xl"></i> Linkedin </a>
+          <a href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=naffzzz@gmail.com" style="text-decoration:none;" class="text-white" target="_blank"><i class="fa-solid fa-at fa-xl"></i> Email </a>
+          <a href="https://github.com/naffzzz" style="text-decoration:none;" class="text-white" target="_blank"><i class="fa-brands fa-github fa-xl"></i> Github </a>
+          <a href="https://gitlab.dot.co.id/naffzzz" style="text-decoration:none;" class="text-white" target="_blank"><i class="fa-brands fa-gitlab fa-xl"></i></i> Gitlab </a>
+        </center>
+      </div>
+    </footer>
+
+    <!-- Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script>
+    function reveal() {
+      var reveals = document.querySelectorAll(".reveal");
+
+      for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+          reveals[i].classList.add("active");
+        }
+        // else {
+        //   reveals[i].classList.remove("active");
+        // }
+      }
+    }
+    window.addEventListener("scroll", reveal);
+    </script>
+
+    <script>
+        $( document ).ready(function() {
+          $("#biographyBtn").click(function() {
+              $('html,body').animate({
+                  scrollTop: $("#biography").offset().top -50},
+                  'slow');
+          });
+
+          $("#educationBtn").click(function() {
+              $('html,body').animate({
+                  scrollTop: $("#education").offset().top -50},
+                  'slow');
+          });
+
+          $("#workExperiencesBtn").click(function() {
+              $('html,body').animate({
+                  scrollTop: $("#workExperiences").offset().top -50},
+                  'slow');
+          });
+
+          $("#programmingBtn").click(function() {
+              $('html,body').animate({
+                  scrollTop: $("#programming").offset().top -50},
+                  'slow');
+          });
+
+          $("#projectExperiencesBtn").click(function() {
+              $('html,body').animate({
+                  scrollTop: $("#projectExperiences").offset().top -50},
+                  'slow');
+          });
+
+          $("#publicationsBtn").click(function() {
+              $('html,body').animate({
+                  scrollTop: $("#publications").offset().top -50},
+                  'slow');
+          });
+
+          $("#awardsBtn").click(function() {
+              $('html,body').animate({
+                  scrollTop: $("#awards").offset().top -50},
+                  'slow');
+          });
+
+          $("#contactMeBtn").click(function() {
+              $('html,body').animate({
+                  scrollTop: $("#contactMe").offset().top -50},
+                  'slow');
+          });
+        });
+    </script>
+  </body>
+</html>
